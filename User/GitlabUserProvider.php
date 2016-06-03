df359b5e56fe1e60a2484a99e644400f24e47c10
Alim Kirem <alim.kirem@otkur.biz>
2016-06-03 10:11:57 +0600

    add user creation

--- User/GitlabUserProvider.php
+++ User/GitlabUserProvider.php
@@ -13,6 +13,86 @@ use Kanboard\User\OAuthUserProvider;
 class GitlabUserProvider extends OAuthUserProvider
 {
     /**
+     * @var bool
+     */
+    protected $allowUserCreation;
+    /**
+     * Constructor
+     *
+     * @access public
+     * @param  array $user
+     * @param  bool   $allowUserCreation
+     */
+    public function __construct(array $user, $allowUserCreation = false)
+    {
+        $this->user = $user;
+        $this->allowUserCreation = $allowUserCreation;
+    }
+    /**
+     * Get username
+     *
+     * @access public
+     * @return string
+     */
+    public function getUsername()
+    {
+        if ($this->allowUserCreation) {
+            return $this->user['username'];
+        }
+
+        return '';
+    }
+    /**
+     * Get name
+     *
+     * @access public
+     * @return string
+     */
+    public function getName()
+    {
+        if ($this->allowUserCreation) {
+            return $this->user['name'];
+        }
+
+        return '';
+    }
+
+    /**
+     * undocumented function
+     *
+     * @return void
+     * @author 
+     **/
+    public function getEmail()
+    {
+        if ($this->allowUserCreation) {
+            return $this->user['email'];
+        }
+
+        return '';
+    }
+    /**
+     * Get Avatar image url from Google profile
+     *
+     * @access public
+     * @return string
+     */
+    public function getAvatarUrl()
+    {
+        return !empty($this->user['avatar_url']) ? $this->user['avatar_url'] : '';
+    }
+    /**
+     * Return true to allow automatic user creation
+     *
+     * @access public
+     * @return boolean
+     */
+    public function isUserCreationAllowed()
+    {
+        return $this->allowUserCreation;
+    }
+
+    /**
      * Get external id column name
      *
      * @access public
@@ -22,4 +102,6 @@ class GitlabUserProvider extends OAuthUserProvider
     {
         return 'gitlab_id';
     }
+
+    
 }
