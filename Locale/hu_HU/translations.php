5813ba6c2bf9c3af24751d748e5bc66ea6d23a90
Balázs Úr <urbalazs@gmail.com>
2018-06-16 18:05:28 +0200

    Updated Hungarian translation

--- Locale/hu_HU/translations.php
+++ Locale/hu_HU/translations.php
@@ -1,11 +1,11 @@
 <?php
 
 return array(
-    // 'Login with my GitLab Account' => '',
-    // 'Link my GitLab Account' => '',
-    // 'Unlink my GitLab Account' => '',
-    // 'GitLab Account' => '',
-    // 'GitLab Authentication' => '',
-    // 'Help on GitLab authentication' => '',
+    'Login with my GitLab Account' => 'Bejelentkezés a GitLab fiókommal',
+    'Link my GitLab Account' => 'Összekapcsolás a GitLab fiókommal',
+    'Unlink my GitLab Account' => 'A GitLab fiókom összekapcsolásának megszüntetése',
+    'GitLab Account' => 'GitLab fiók',
+    'GitLab Authentication' => 'GitLab hitelesítés',
+    'Help on GitLab authentication' => 'Súgó a GitLab hitelesítéshez',
 );
 
